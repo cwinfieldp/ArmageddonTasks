5170e24857dd06c77cbd5d041f830db2be29d5d9
cwinfieldp <cwinfieldp7@gmail.com>
2024-05-13 23:56:25 -0500

    Task 1 for Armageddon

---
+++ task1.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.25.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  project = "gcp-terraform-project-420321"
+  region = "us-central1"
+  zone = "us-central1-a"
+  credentials = "gcp-terraform-project-420321-f2cc606dad59.json"
+}
+
+resource "google_storage_bucket" "armageddon-bucket3" {
+  name          = "armageddon-bucket3"
+  location      = "US"
+  force_destroy = true
+}
+
+  resource "google_storage_bucket_iam_binding" "armageddon-bucket3" {
+  bucket = google_storage_bucket.armageddon-bucket3.name
+  role   = "roles/storage.objectViewer"
+
+  members = [
+    "allUsers",
+  ]
+}
+resource "google_storage_bucket_object" "object" {
+    name   = "index.html"
+    bucket = google_storage_bucket.armageddon-bucket3.name
+    source = "/terraform/Armageddon/Task1/Index.html"
+    content_type = "text/html"
+    
+    depends_on = [
+        google_storage_bucket.armageddon-bucket3,
+        google_storage_bucket_object.object,
+    ]
+}
+
+output "public_url" {
+  value = "https://storage.googleapis.com/${google_storage_bucket.armageddon-bucket3.name}/${google_storage_bucket_object.object.name}"
+  description = "The public URL to access the index.html"
+}
