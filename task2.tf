4b1ace09ae3356e5bcbcf9a33fbd0d56efd5e39d
Caleb Peterson <149834667+cwinfieldp@users.noreply.github.com>
2024-05-14 00:07:42 -0500

    Update task2.tf

--- task2.tf
+++ task2.tf
@@ -12,7 +12,7 @@ provider "google" {
   project = "gcp-terraform-project-420321"
   region = "us-central1"
   zone = "us-central1-a"
-  credentials = "gcp-terraform-project-420321-f2cc606dad59.json"
+  credentials = ""
 }
 resource "google_compute_network" "gcpgentleman_custom_vpc" {
   name                    = "gcpgentleman-custom-vpc"
